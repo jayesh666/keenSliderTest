c2281bf7e0d7b4c7b49da02267ec3874e076997e
JayeshKathrotiya <jayesh.kathrotiya@mymobioffice.com>
2023-10-30 12:49:43 +0530

    keen slider demo test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import KeenSlider, { KeenSliderInstance } from "keen-slider"
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [
+    "../../node_modules/keen-slider/keen-slider.min.css",
+    './app.component.css']
 })
 export class AppComponent {
+
+  @ViewChild("sliderRef") sliderRef!: ElementRef<HTMLElement>
+
+  currentSlide: number = 1
+  dotHelper: Array<Number> = []
+  slider: (KeenSliderInstance | null) = null
+
   title = 'slider';
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.slider = new KeenSlider(this.sliderRef.nativeElement, {
+        initial: this.currentSlide,
+        slideChanged: (s) => {
+          this.currentSlide = s.track.details.rel
+        },
+      })
+      this.dotHelper = [
+        ...Array(this.slider.track.details.slides.length).keys(),
+      ]
+    })
+  }
+
+  ngOnDestroy() {
+    if (this.slider) this.slider.destroy()
+  }
 }
